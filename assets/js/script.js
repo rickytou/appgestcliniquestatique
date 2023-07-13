0c0bd70a3253c5b25419ad93176935ce7ac9dd00
Ricardo Samedi <samedi.ricardo@gmail.com>
2023-07-13 02:18:12 -0400

    Integration de la section equipe, temoignages, services, galeries, contact et footer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,11 @@ $(function(){
     $(this).removeClass('active');
   })
 
+/**
+ * Ajout de la classe active quand l'utilisateur clique sur un lien de menu
+ */
+
+
   /**
    * Ajout de la classe active quand le curseur passe sur un membre de l'equipe 
    */
@@ -34,4 +39,68 @@ $(function(){
   $('.equipe__bloc').on('mouseleave',function(){
     $(this).removeClass('active');
   })
+
+  /**
+   * Focus sur la section clinique
+   */
+  window.addEventListener('scroll',function(){
+    const clinique = document.querySelector("#clinique");
+    const equipe = document.querySelector("#equipe");
+    const services = document.querySelector("#services");
+    const galeries = document.querySelector("#galeries");
+    const contact = document.querySelector("#contact");
+    const minus = 220;
+    $('.menu li').removeClass('active');
+    if(window.scrollY >= (equipe.offsetTop - minus) && this.window.scrollY <= (equipe.offsetTop + equipe.scrollHeight)){
+        //$('.menu li').removeClass('active');
+       $('a[href="#equipe"]').parent().addClass('active');        
+    }
+    else if(window.scrollY >= (clinique.offsetTop - minus) && this.window.scrollY <= (clinique.offsetTop + clinique.scrollHeight)){
+      $('.menu li').removeClass('active');
+      $('a[href="#clinique"]').parent().addClass('active');
+      $('.sectionCliniqueGroup__:nth-child(1)').addClass('show'); 
+      $('.sectionCliniqueGroup__:nth-child(2)').addClass('show');
+      $('.sectionCliniqueGroup__:nth-child(3)').addClass('show'); 
+      $('.sectionCliniqueGroup__:nth-child(4)').addClass('show');
+    }
+    else if(window.scrollY >= (services.offsetTop - minus) && this.window.scrollY <= (services.offsetTop + services.scrollHeight)){
+      //$('.menu li').removeClass('active');
+      $('a[href="#services"]').parent().addClass('active'); 
+    } 
+    else if(window.scrollY >= (galeries.offsetTop - minus) && this.window.scrollY <= (galeries.offsetTop + galeries.scrollHeight)){
+      //$('.menu li').removeClass('active');
+      $('a[href="#galeries"]').parent().addClass('active'); 
+    }
+    else if(window.scrollY >= (contact.offsetTop - minus) && this.window.scrollY <= (contact.offsetTop + contact.scrollHeight)){
+      //$('.menu li').removeClass('active');
+      $('a[href="#contact"]').parent().addClass('active'); 
+    }
+    //  console.log(window.scrollY, "/" ,equipe.offsetTop);
+  });
+
+  /**
+   * Defilement des temoignages
+   */
+  
+  $('.temoignages__navigation').on('click',function(){
+    var temoignages = $('.temoignages__bloc__group');
+    var compteur;
+    var next;
+    $.each(temoignages, function(index, temoignage){
+     if(temoignage.classList.contains('active')){
+      temoignage.classList.remove('active');
+      compteur = index;
+    }
+
+  });
+  if(compteur < temoignages.length - 1){
+  next = temoignages[compteur + 1];
+  }
+  else{
+    next = temoignages[0];
+  }
+ 
+  console.log(next);
+  next.classList.add('active');
+});
 });
--- index.php
+++ index.php
@@ -25,7 +25,7 @@ declare(strict_types = 1);
           <!-- Logo et Nom clinique-->
             <div class="logo">
               <img src="assets/images/logo-clinique-jeannot-cadet.png" alt="logo clinique" />
-              <h1>Clinique Jeannot Cadet</h1>    
+              <h1><a href="#headerMiddle">Clinique Jeannot Cadet</a></h1>    
             </div>
             <!--Menuburger -->
             <div class="menuburger">
@@ -35,25 +35,41 @@ declare(strict_types = 1);
             </div>
             <!-- Menu -->
             <ul class="menu">
-              <li class="active"><a href="#"><span>Clinique</span><span class="fa-solid fa-house-chimney-medical"></span></a></li>
-              <li><a href="#"><span>Equipe</span><span class="fa-solid fa-people-group"></span></a></li>
-              <li><a href="#"><span>Services</span><span class="fa-solid fa-wrench"></span></a></li>
-              <li><a href="#"><span>Galeries</span><span class="fa-regular fa-image"></span></a></li>
-              <li><a href="#"><span class="menuLinkHidden">Contact</span><span class="fa-regular fa-envelope"></span></a></li>
-              <li><a href="#"><span class="menuLinkHidden">Connexion</span><span class="fa-regular fa-circle-user"></span></a></li>
-              <li><a href="#"><span class="menuLinkHidden">T&eacute;moignages</span><span class="fa-regular fa-comments"></span></a></li>
+              <li>
+                <a href="#clinique"><span>Clinique</span><span class="fa-solid fa-house-chimney-medical"></span></a>
+              </li>
+              <li>
+                <a href="#equipe"><span>Equipe</span><span class="fa-solid fa-people-group"></span></a>
+              </li>
+              <li>
+                <a href="#services"><span>Services</span><span class="fa-solid fa-wrench"></span></a>
+              </li>
+              <li>
+                <a href="#galeries"><span>Galeries</span><span class="fa-regular fa-image"></span></a>
+              </li>
+              <li>
+                <a href="#temoignages"><span class="menuLinkHidden">T&eacute;moignages</span><span class="fa-regular fa-comments"></span></a>
+              </li>
+              <li>
+                <a href="#contact"><span class="menuLinkHidden">Contact</span><span class="fa-regular fa-envelope"></span>
+                </a>
+              </li>
+              <li>
+                <a href="#"><span class="menuLinkHidden">Connexion</span><span class="fa-regular fa-circle-user"></span>
+              </a>
+              </li>
             </ul>
           </div>
         </nav>
       </div>
       <!-- Container Navigation Global -->
-     <div class="headerMiddle">
+     <div class="headerMiddle" id="headerMiddle">
       <!-- Images illustratives -->
         <img src="assets/images/docteur.svg" />
       <!-- Phrase d'accrode -->
       <div class="accroche">
         <!-- <strong>Virage au num&eacute;rique</strong> -->
-        <strong>Traitement m&eacute;dical et chirurgical de qualit&eacute;</strong>
+        <strong>Traitement m&eacute;dical et <br />chirurgical de qualit&eacute;</strong>
         <h2>Leader et pionnier du domaine</h2>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit sit aliquam, fuga blanditiis vero nemo repudiandae accusamus porro pariatur quod?</p>
         <a href="#" class="btn-accroche"> Suivi m&eacute;dical</a>
@@ -248,6 +264,189 @@ declare(strict_types = 1);
       
     </section>
     <!-- Fin Service -->
+    <section id="galeries">
+      <div class="galeries">
+        <p class="galeries__img_1"><img src="assets/images/img-1.jpeg"</p>
+        <p class="galeries__img_2"><img src="assets/images/img-2.jpeg"</p>
+        <p class="galeries__img_3"><img src="assets/images/img-3.jpeg"</p>
+        <p class="galeries__img_4"><img src="assets/images/img-4.jpeg"</p>
+        <p class="galeries__img_5"><img src="assets/images/img-5.jpeg"</p>
+        <p class="galeries__img_6"><img src="assets/images/img-6.jpeg"</p>
+        <p class="galeries__img_7"><img src="assets/images/img-7.jpeg"</p>
+        <p class="galeries__img_8"><img src="assets/images/img-8.jpeg"</p>
+        <p class="galeries__img_9"><img src="assets/images/img-9.jpeg"</p>
+        <p class="galeries__img_10"><img src="assets/images/img-10.jpeg"</p>
+      </div>
+    </section>
+     <!-- Temoignages -->
+     <section id="temoignages">
+      <div class="temoignages">
+        <h3>
+          Ce que disent <span>nos visiteurs ?</span>
+        </h3>
+        <!-- Temoignages [Zone de navigation] -->
+        <p class="temoignages__navigation">
+          <i class="fa-solid fa-arrow-right"></i>
+        </p>
+        <!-- Bloc de temoignages -->
+        <div class="temoignages__bloc">
+          <!-- Temoignages comments [Temoignages1] -->
+          <div class="temoignages__bloc__group active">
+              <div class="temoignages__bloc__comments">
+                <div class="temoignages__bloc__comments_user">
+                  <blockquote>
+                  <i class="fa-solid fa-quote-left"></i>
+                    <p>CLorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.
+                    </p>
+                    <i class="fa-solid fa-quote-right"></i>
+                    <cite>
+                      <span>John Doe</span>
+                    </cite>
+                  </blockquote>
+                </div>
+              </div>
+              <!-- Temoignages bloc design -->
+              <div class="temoignages__bloc__design"></div>
+              <!-- Temoignages profil de l'utilisateur -->
+              <div class="temoignages__bloc__profil">
+                  <img src="assets/images/avatar.png" alt="avatar"/>    
+              </div>
+            </div>
+          <!-- Fin -->
+
+          <!-- Temoignages comments [Temoignages2] -->
+          <div class="temoignages__bloc__group">
+              <div class="temoignages__bloc__comments">
+                <div class="temoignages__bloc__comments_user">
+                  <blockquote>
+                  <i class="fa-solid fa-quote-left"></i>
+                    <p>VLorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.
+                    </p>
+                    <i class="fa-solid fa-quote-right"></i>
+                    <cite>
+                      <span>Johnny Doe</span>
+                    </cite>
+                  </blockquote>
+                </div>
+              </div>
+              <!-- Temoignages bloc design -->
+              <div class="temoignages__bloc__design"></div>
+              <!-- Temoignages profil de l'utilisateur -->
+              <div class="temoignages__bloc__profil">
+                  <img src="assets/images/avatar.png" alt="avatar"/>    
+              </div>
+            </div>
+          <!-- Fin -->
+
+          <!-- Temoignages comments [Temoignages3] -->
+          <div class="temoignages__bloc__group">
+              <div class="temoignages__bloc__comments">
+                <div class="temoignages__bloc__comments_user">
+                  <blockquote>
+                  <i class="fa-solid fa-quote-left"></i>
+                    <p>JLorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.
+                    </p>
+                    <i class="fa-solid fa-quote-right"></i>
+                    <cite>
+                      <span>Jane Doe</span>
+                    </cite>
+                  </blockquote>
+                </div>
+              </div>
+              <!-- Temoignages bloc design -->
+              <div class="temoignages__bloc__design"></div>
+              <!-- Temoignages profil de l'utilisateur -->
+              <div class="temoignages__bloc__profil">
+                  <img src="assets/images/avatar.png" alt="avatar"/>    
+              </div>
+            </div>
+          <!-- Fin -->
+
+          <!-- Temoignages comments [Temoignages4] -->
+          <div class="temoignages__bloc__group">
+              <div class="temoignages__bloc__comments">
+                <div class="temoignages__bloc__comments_user">
+                  <blockquote>
+                  <i class="fa-solid fa-quote-left"></i>
+                    <p>JLorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.
+                    </p>
+                    <i class="fa-solid fa-quote-right"></i>
+                    <cite>
+                      <span>Spence Doe</span>
+                    </cite>
+                  </blockquote>
+                </div>
+              </div>
+              <!-- Temoignages bloc design -->
+              <div class="temoignages__bloc__design"></div>
+              <!-- Temoignages profil de l'utilisateur -->
+              <div class="temoignages__bloc__profil">
+                  <img src="assets/images/avatar.png" alt="avatar"/>    
+              </div>
+            </div>
+          <!-- Fin -->
+
+
+     </section>
+     <!-- Contact -->
+     <section id="contact">
+      <div class="contact">
+        <h3>Nous contacter</h3>
+        <div class="contact__bloc">
+          <form>
+            <div class="contact__bloc__left">
+                <textarea></textarea>
+                <p>
+                  <img src="assets/images/Feedback-amico.svg" />
+                </p>
+            </div>  
+            <div class="contact__bloc__middle">
+                <input type="text" placeholder="Votre nom">
+                <input type="email" placeholder="Votre email">
+                <input type="tel" placeholder="Telephone [Optionnel]">
+                <input type="submit" value="Envoyer">
+            </div>
+              <address class="contact__bloc__right">
+                <p>
+                  <strong>Clinique Jeannot Cadet</strong>
+                </p>
+                <p>
+                <i class="fa-solid fa-location-dot"></i>
+                  <span>180, Avenue Lamartini&egrave;re</span><br />
+                  <span>Port-au-Prince, Haiti HT6112</span>
+                </p>
+                <p>
+                  <i class="fa-solid fa-phone"></i>
+                  <span>(509) 3122-1122</span>
+                </p>
+                <p>
+                  <i class="fa-solid fa-envelope"></i>
+                  <span>jeannotcadetclinique@gmail.com</span>
+                </p>
+              </address>
+          </form>
+            
+        </div>
+      </div>
+     </section>
+     <!-- Footer -->
+     <footer id="footer">
+      <div class="footer">
+        <p class="footer__social">
+          <i class="fa-brands fa-facebook-f"></i>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-linkedin-in"></i>
+          <i class="fa-brands fa-youtube"></i>
+         </p>
+        <ul class="menufooter">
+          <li><a href="#">Misson</a></li>
+          <li><a href="#">Partenaires</a></li>
+          <li><a href="#">Carri&egrave;res</a></li>
+          <li><a href="#">Termes et conditions</a></li>  
+        </ul>
+        <p class="footer__copyright"><span>Clinique Jeannot Cadet@2023</span><span> Tous droits r&eacute;serv&eacute;s</span> <span>|</span> <span>Conception par : Ricardo Samedi</span></p>
+      </div>
+     </footer>
   </div>
 
 
