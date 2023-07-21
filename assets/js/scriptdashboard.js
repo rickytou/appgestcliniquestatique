49da4df7e6202b2428e5709aa738b56d2c3f73de
Ricardo Samedi <samedi.ricardo@gmail.com>
2023-07-21 06:21:58 -0400

    Mises a jour du dossier de projet

--- assets/js/scriptdashboard.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $('.theme').on('click', function(){
-    $(this).toggleClass('active');
-    $('.headerdashboard').toggleClass('active');
-  });
-  $('.dashboard__link__submenu').on('click', function(){
-    $('.dashboard__container').toggleClass('active__submenu');
-  });
-});
